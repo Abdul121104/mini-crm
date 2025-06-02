5a50cfb722f41c14ce00ae4067f67daf42725808
Abdul121104 <119029386+Abdul121104@users.noreply.github.com>
2025-06-02 19:57:26 +0530

    change

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://mini-crm-backend-ngzd.onrender.com"
+    callbackURL: "/api/auth/google/callback"
   },
   async function(accessToken, refreshToken, profile, done) {
     try {
